dcc53841e3d8c36abf910ca5f8f23be8c846b3ec
Moritz Wilksch <moritz.wilksch@duke.edu>
2021-11-04 01:27:10 -0400

    twint mvp

---
+++ scripts/.twint_mvp.py
@@ -0,0 +1,12 @@
+import twint
+
+c = twint.Config()
+
+c.Search = "#great"  # your search here
+c.Since = "2021-10-31 00:00:00"
+c.Until = "2021-11-01 00:00:00"
+
+c.to_csv = True
+c.Output = "twint_out.csv"
+
+twint.run.Search(c)
