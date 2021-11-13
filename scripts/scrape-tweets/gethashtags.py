89d0ca2f866fd9c41ccdf5834280d4181bee5df2
unsupervisedlearner1123 <deekshitasaikia@gmail.com>
2021-11-13 14:02:25 -0500

    Updated hastag fetch

---
+++ scripts/scrape-tweets/gethashtags.py
@@ -0,0 +1,18 @@
+import os
+import requests
+
+access_token = os.environ.get("API_TOKEN")
+
+url = "https://api.twitter.com/1.1/trends/place.json?id=23424977"
+
+payload = {}
+headers = {
+    "Authorization": "Bearer {}".format(access_token),
+    # "Cookie": 'guest_id=v1%3A163682872111862049; personalization_id="v1_NsvQEMNM+EmjK0Lkr4PpUw=="',
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+tweet_data = response.json()
+for i in range(0, 10):
+    print(tweet_data[0]["trends"][i]["name"])
