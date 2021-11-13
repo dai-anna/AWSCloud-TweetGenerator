277f6f360563f9e309030859a2e567e05f894112
Moritz Wilksch <moritz.wilksch@duke.edu>
2021-11-13 18:03:01 -0500

    dockerize in progress

--- Makefile
+++ Makefile
@@ -11,13 +11,12 @@ docker-build:
 	rm ./scripts/scrape-tweets/requirements.txt
 
 docker-run:
-	docker run -d --name datacollector datacollector
+	docker run -d --name datacollector -e API_TOKEN=${API_TOKEN} datacollector
 
 docker-run-debug:
-	docker run -it --name datacollector datacollector
+	docker run -it --name datacollector -e API_TOKEN=${API_TOKEN} datacollector
 	docker cp datacollector:/app/twint_out.csv ~/AWSCloud-TweetGenerator/twint_out.csv
 	
-
 docker-clean:
 	docker rm -f datacollector
 	
--- scripts/scrape-tweets/gethashtags.py
+++ scripts/scrape-tweets/gethashtags.py
@@ -11,12 +11,21 @@ headers = {
     # "Cookie": 'guest_id=v1%3A163682872111862049; personalization_id="v1_NsvQEMNM+EmjK0Lkr4PpUw=="',
 }
 
+
 response = requests.request("GET", url, headers=headers, data=payload)
+
+if not response.ok:
+    print(f"[ERROR] Request to Twitter answered with {response.status_code}")
+    print(response.text)
 
 tweet_data = response.json()
 
 trends_ls = []
 
 for i in range(0, 10):
-    trends_ls.append(tweet_data[0]["trends"][i]["name"])
+    try:
+        trends_ls.append(tweet_data[0]["trends"][i]["name"])
+    except KeyError:
+        print("[ERROR] Unexpected JSON structure. Got JSON:")
+        print(tweet_data)
     # print(tweet_data[0]["trends"][i]["name"])
