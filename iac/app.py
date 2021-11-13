aecbf271aeca244232a69f5aa21a0f775f557e6a
Satvik Kishore <satvik.kishore@gmail.com>
2021-11-13 21:48:01 +0000

    advanced iac

--- iac/app.py
+++ iac/app.py
@@ -26,7 +26,7 @@ IacStack(app, "IacStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=core.Environment(account='123456789012', region='us-east-1'),
+    env=core.Environment(account='533527479286', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -8,9 +8,41 @@ class IacStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
         vpc = aws_ec2.Vpc(self, "main-vpc")
-        batch_compute_resources = aws_batch.ComputeResources(aws_ec2.Vpc.from_lookup(self, "main-vpc"))
-        batch_compute_env = aws_batch.ComputeEnvironment(self, 'batch-compute-env', batch_compute_resources)
+        #print(vpc.public_subnets[0].subnet_id)
+        #print(vpc.private_subnets)
+        #print(vpc.isolated_subnets)
+        i_vpc = aws_ec2.Vpc.from_vpc_attributes(self,
+            "main-ipvc", 
+            availability_zones = [i.availability_zone for i in vpc.public_subnets], 
+            vpc_id = "main-vpc",
+            public_subnet_ids = [i.subnet_id for i in vpc.public_subnets],
+        )
+        batch_compute_resources = aws_batch.ComputeResources(
+            vpc = i_vpc,
+            desiredv_cpus = 1,
+            maxv_cpus = 1,
+            minv_cpus = 0,
+        )
+        batch_compute_env = aws_batch.ComputeEnvironment(
+            scope=self,
+            id='batch-compute-env',
+            compute_resources=batch_compute_resources,
+        )
+        q_batch_compute_env = aws_batch.JobQueueComputeEnvironment(
+            aws_batch.ComputeEnvironment.from_compute_environment_arn(
+                self,
+                id="i-compute-env",
+                compute_environment_arn = batch_compute_env.compute_environment_arn,
+            ),
+            order = 1,
+        )
+        batch_queue = aws_batch.JobQueue(
+            self,
+            "batch-queue",
+            compute_environments = q_batch_compute_env,
+        )
         pass
     pass
+
+#availability_zone
