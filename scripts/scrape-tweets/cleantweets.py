c7a8568aac2e8fcce33ba686084b623a8409008d
EC2 Default User <ec2-user@ip-172-31-2-74.us-east-2.compute.internal>
2021-11-14 03:00:33 +0000

    prep for processing

--- scripts/scrape-tweets/cleantweets.py
+++ scripts/scrape-tweets/cleantweets.py
@@ -1,10 +1,45 @@
-#%%
-
 import pandas as pd
-# from gethashtags import trends_ls
+import os
+import re
+import boto3
 
-df = pd.read_csv("twint_out_1.csv", sep = ",")
-df["tweet"].to_list()
-df.columns
-df[df["language"] == "en"]
-# %%
+ROOT_DIR = "./"
+
+filePath = f"{ROOT_DIR}hashtags.txt"
+
+if os.path.exists(filePath):
+    os.remove(filePath)
+else:
+    pass
+
+s3 = boto3.resource(
+    service_name="s3",
+    region_name="us-east-1",
+    aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
+)
+
+
+bucket = s3.Bucket("exp-noahgift")
+bucket.download_file("hashtags.txt", f"{ROOT_DIR}hashtags.txt")
+print("[INFO] Hashtags fetched from S3.")
+
+with open(f"{ROOT_DIR}hashtags.txt") as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+tweet_files = os.listdir()
+
+def load_files(filenames):
+    for file in filenames:
+        if file.startswith('twint_out'):
+            tag = lines[int(re.findall("\d+",file)[0])]
+            tag_idx = int(re.findall("\d+",file)[0])
+            df = pd.read_csv(file, sep=",", usecols = ['tweet'])
+            df["tag"] = tag.strip("#")
+            df["tag_idx"] = tag_idx
+            yield df
+        
+all_tweets_raw = pd.concat(load_files(tweet_files))
+
+print(all_tweets_raw.head())
