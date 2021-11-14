e8958b6366e6456276911e061adeb1165b7bf018
Anna Dai <anna.dai@duke.edu>
2021-11-13 23:07:15 -0500

    regex

--- scripts/scrape-tweets/cleantweets.py
+++ scripts/scrape-tweets/cleantweets.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import re
 import boto3
+import tempfile
 
 ROOT_DIR = "./"
 
@@ -44,7 +45,35 @@ def load_files(filenames):
             yield df
         
 all_tweets_raw = pd.concat(load_files(tweet_files))
-all_tweets_raw.to_csv(f"{ROOT_DIR}raw_tweets.csv")
+# all_tweets_raw.to_csv(f"{ROOT_DIR}raw_tweets.csv")
+
+emoji_pattern = re.compile("["
+    u"\U0001F600-\U0001F64F"  # emoticons
+    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    u"\U00002702-\U000027B0"
+    u"\U000024C2-\U0001F251"
+    "]+", flags=re.UNICODE)
+
+url_mentions = re.compile(r'(@\S+) | (https?:\/\/.+)')
+
+all_tweets_raw["tweet"] = all_tweets_raw["tweet"].replace(emoji_pattern, '', regex=True)
+all_tweets_raw["tweet"] = all_tweets_raw["tweet"].replace(url_mentions, '', regex=True)
+all_tweets_raw["tweet"] = all_tweets_raw["tweet"].replace("#", '', regex=True)
 
 
-print(all_tweets_raw.head())
+# for idx in all_tweets_raw["tag_idx"].unique():
+#     df = all_tweets_raw.loc[all_tweets_raw["tag_idx"]==idx].copy()
+#     df = df["tweet"]
+
+with tempfile.TemporaryFile() as fp:
+    all_tweets_raw.to_csv(fp)
+    fp.seek(0)
+    bucket.upload_fileobj(fp, "trash.csv")
+
+# bucket.upload_file(f"clean_out_{idx}.csv", bucket)
+
+
+
+# print(all_tweets_raw.head())
